2f51b06abfd76e147951b3fdd82738d6ed972053
j7lmo <julien.lcx@gmail.com>
2022-03-29 11:41:17 +0200

    init

--- frontend/src/Shared/NavBar/NavBar.tsx
+++ frontend/src/Shared/NavBar/NavBar.tsx
@@ -16,27 +16,28 @@ export class NavBar extends React.Component<NavBarProps>
     public render ( )
     {
         return (
+            
             <div className='NavBar'>
                 <div
                     className='NavBar-item'
                     onClick={this.props.onSelectProfile}>
                     Profiles
                 </div>
+                <div
+                    className='NavBar-item'
+                    onClick={this.props.onAddObject}>
+                    Upload a file 
+                </div>
+            
+                <div className='NavBar-spacer'></div>
                 
                 <div
                     className='NavBar-item'
                     onClick={this.props.onAbout}>
                     About
                 </div>
-
-                <div className='NavBar-spacer'></div>
-
-                <div
-                    className='NavBar-item'
-                    onClick={this.props.onAddObject}>
-                    Upload a file
                 </div>
-            </div>
+                
         );
     }
 }
